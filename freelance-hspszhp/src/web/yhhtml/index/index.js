d1497c7e351993653438267aa60093b859a4c54c
13632225945 <969407578@qq.com>
2017-12-24 18:59:21 +0800

    增加菜单收缩功能，增加三级菜单
    增加菜单收缩功能，增加三级菜单

--- freelance-hspszhp/src/web/yhhtml/index/index.js
+++ freelance-hspszhp/src/web/yhhtml/index/index.js
@@ -3,6 +3,18 @@
 		e.stopPropagation();
 		$(this).children('ul').slideToggle();
 	});;
+	
+	$('#toggleLeft').click(function() {
+		if($(this).find('i.fa').hasClass('fa-arrow-left')) {
+			$('.mho_left_menu').animate({'left': '-200px'});
+			$('.mho_center').animate({'left': '0'});
+			$(this).find('i.fa').addClass('fa-arrow-right').removeClass('fa-arrow-left');
+		}else {
+			$('.mho_left_menu').animate({'left': '0'});
+			$('.mho_center').animate({'left': '200px'});
+			$(this).find('i.fa').addClass('fa-arrow-left').removeClass('fa-arrow-right');
+		}
+	});
 
 	var ctx1 = document.getElementById("barChart").getContext("2d");
 	var data1 = {
